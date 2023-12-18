0bfdf478e02780afac7c9c9e4edaabf9bcc108b8
Nino Cartesiano <nino.cartesiano@rafi.ph>
2023-12-18 13:18:01 +0800

    login ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Signup from './pages/signup/Signup';
+import RouterDom from './routes/routes';
 
 function App() {
   return (
     <div style={{background: '#121212', height: '100vh'}}>
-      <Signup/>
+      <RouterDom/>
     </div>
   );
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import askmeai_logo from '../../assets/askmeai_logo.png'
 import ctu_logo from '../../assets/ctu_logo.png'
 import open_ai_logo from '../../assets/open_ai_logo.png'
-import { Form } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-function SignUpForm() {
+function LoginForm() {
     return (
         <Form>
             <Form.Group className="mb-3">
@@ -17,19 +17,27 @@ function SignUpForm() {
                 <Form.Label style={{color: 'white'}}>Password</Form.Label>
                 <Form.Control type="password" style={{width: '100%'}}/>
             </Form.Group>
-            <section style={{display: 'flex', gap: '1rem', justifyContent: 'center'}}>
-                <Form.Check name='regType' type='radio' label="Teacher" style={{color: 'white'}}/>
-                <Form.Check name='regType' type='radio' label="Student" style={{color: 'white'}}/>
-                <Form.Check name='regType' type='radio' label="Others" style={{color: 'white'}}/>
-            </section>
         </Form>
     )
 }
 
-function Signup() {
+function Login() {
   return (
     <>
         <div style={{display: 'flex',padding: '4rem', paddingTop: '3rem'}}>
+            <section style={{width: '50%', display: 'flex', justifyContent: 'center'}}>
+                <div style={{width: '20rem'}}>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <img src={askmeai_logo} width='200px' alt="askmeai_logo"/>
+                    </div>
+                    <h2 style={{color: 'white', fontSize: '1.5rem', textAlign: 'center'}}>Login to your account</h2>
+                    <LoginForm /><br />
+                    <div style={{display: 'flex', gap: '1rem', color: 'white', justifyContent: 'center'}}>
+                        <p >Don't have an account?</p>
+                        <Link to='/SignUp' style={{color: '#2CF6B3'}}>Sign Up</Link>
+                    </div>
+                </div>
+            </section>
             <section style={{width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <div style={{color: 'white', textAlign: 'center', zIndex: '1'}}>
                     <h1 style={{fontSize: '3.5rem'}}>Welcome to Ask Me AI </h1>
@@ -40,24 +48,10 @@ function Signup() {
                     </div>
                 </div>
             </section>
-            <section style={{width: '50%', display: 'flex', justifyContent: 'center'}}>
-                <div style={{width: '20rem'}}>
-                    <div style={{display: 'flex', justifyContent: 'center'}}>
-                        <img src={askmeai_logo} width='200px' alt="askmeai_logo"/>
-                    </div>
-                    <h2 style={{color: 'white', fontSize: '1.5rem', textAlign: 'center'}}>Create your account</h2>
-                    <SignUpForm /><br />
-                    <div style={{display: 'flex', gap: '1rem', color: 'white', justifyContent: 'center'}}>
-                        <p >Already have an account?</p>
-                        <Link to='/' style={{color: '#2CF6B3'}}>Log In</Link>
-                    </div>
-                </div>
-            </section>
             
         </div>
     </>
-    
   )
 }
 
-export default Signup
+export default Login
