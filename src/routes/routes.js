8df65f10eaddfe5d6d74a03e5e2d0bcb289ad016
Nino Cartesiano <nino.cartesiano@rafi.ph>
2023-12-28 08:56:35 +0800

    fix page not deploying

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from '../pages/signup/Signup';
 import Login from '../pages/login/Login';
 import Homepage from '../pages/homepage/Homepage';
-import Chat from '../pages/chat/Chat'
+import Chat from '../pages/chat/Chat';
+
 const RouterDom = () => {
  return (
     <>
